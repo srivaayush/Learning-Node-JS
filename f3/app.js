d8ee1cf65302572a19cb3b4f83dcd5e85cf11aa9
Ayush srivastava <110943974+srivaayushCN@users.noreply.github.com>
2022-12-10 00:35:16 +0530

    block 3 complete

--- f3/app.js
+++ f3/app.js
@@ -83,27 +83,30 @@ server.listen(3000);
 */
 
 // video 10 redirecting requests
+/*
 const fs = require("fs");
 const server = http.createServer((req, res) => {
   //   console.log(req.url, req.method, req.headers);
   const url = req.url;
   const method = res.method;
 
-  if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title> Enter Message </title></head>");
+  if (url === '/') {
+    // res.setHeader("Content-Type", "text/html");
+    res.write('<html>');
+    res.write('<head><title> Enter Message </title></head>');
     res.write(
-      '<body><form action="/message" method="POST"> <input type ="text"> <button type="submit"> Send </button> </form></body>'
+      '<body><form action="/message" method="POST"> <input type ="text" name="message"> <button type="submit"> Send </button> </form></body>'
     );
     res.write("</html>");
     return res.end();
   }
 
-  if (url == "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "Dummy");
+  if (url === '/message' && method === 'POST') {
+    console.log('here');
+    fs.writeFileSync('message.txt', 'Dummy');
     res.statusCode = 302;
-    res.setHeader("Location", "/");
+    res.setHeader('Location', '/');
+    console.log('here end');
     return res.end();
   }
 
@@ -112,6 +115,159 @@ const server = http.createServer((req, res) => {
   res.write("<head><title>My first Page</title></head>");
   res.write("<body><h1> Hello!!!! </h1></body>");
   res.write("</html>");
+});
+
+server.listen(3000);
+*/
+
+// Video11 parsing request bodies
+/*
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  //   console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  const method = res.method;
+
+  if (url === "/") {
+    // res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title> Enter Message </title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+
+      fs.writeFileSync('message.txt', message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My first Page</title></head>");
+  res.write("<body><h1> Hello!!!! </h1></body>");
+  res.write("</html>");
+});
+
+server.listen(3000);
+*/
+
+
+//video 12 event driven code
+/*
+const fs = require('fs');
+const server = http.createServer((req, res) => {
+  //   console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  const method = res.method;
+
+  if (url === '/') {
+    // res.setHeader("Content-Type", "text/html");
+    res.write('<html>');
+    res.write('<head><title> Enter Message </title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split('=')[1];
+
+      fs.writeFileSync('message.txt', message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+
+    return res.end();
+  }
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My first Page</title></head>');
+  res.write('<body><h1> Hello!!!! </h1></body>');
+  res.write('</html>');
+});
+
+server.listen(3000);
+*/
+
+// video 13 Blocking and non blocking code
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  //   console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  const method = res.method;
+
+  if (url === '/') {
+    // res.setHeader("Content-Type", "text/html");
+    res.write('<html>');
+    res.write('<head><title> Enter Message </title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+     res.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split('=')[1];
+
+      fs.writeFile('message.txt', message,err=>{
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+    
+        return res.end();
+      });
+    });
+
+    
+  }
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My first Page</title></head>');
+  res.write('<body><h1> Hello!!!! </h1></body>');
+  res.write('</html>');
 });
 
 server.listen(3000);
